752c573c9e94889b34b2683d864c21ba0880e6a3
mosheshulman <msh36345@gmail.com>
2025-07-22 18:37:28 +0300

    create validator class

---
+++ NaiveBayes-v2.0/Validater.py
@@ -0,0 +1,48 @@
+from Log import log
+from Trainer import Trainer
+
+class Validator:
+    def __init__(self,df):
+        self.df = df
+        self.calculate_70 = int(len(self.df)*0.7)
+        self.column_to_fill = self.df.columns.to_list()[-1]
+        log(f"Start testing : train {self.calculate_70} rows | test : {len(self.df)-self.calculate_70} rows")
+        self.train_dic = self.create_train_df()
+        self.test_dic = self.create_test_df()
+        self.tester()
+
+    def tester(self):
+        counter = 0
+        rows_counter = 0
+        for row_num, row_val in self.test_dic.items():
+            check = self.run_validation(row_val, self.train_dic)
+            answer = max(check, key=check.get)
+            # log(f"answer row {row_num} : {answer}")
+            rows_counter += 1
+            if answer == row_val[self.column_to_fill]:
+                counter += 1
+        log(f"{counter}/{rows_counter}")
+        res = (counter / rows_counter) * 100
+        return f"{round(res, 2)}%"
+
+    def run_validation(self,row_val,train_dic):
+        res = {}
+        for target_value,unique_word_counts in train_dic.items():
+            res_num = 1
+            for col_value, sum_value in row_val.items():
+                if col_value == self.column_to_fill:
+                    continue
+                else:
+                    res_num *= unique_word_counts[col_value][sum_value]
+            res[target_value] = res_num
+        return res
+
+    def create_train_df(self):
+        df_70 = self.df.iloc[:self.calculate_70]
+        trainer = Trainer(df_70)
+        return trainer.get_calculate_data()
+
+    def create_test_df(self):
+        thirty_percent_df = self.df.iloc[self.calculate_70:]
+        thirty_percent_dict = thirty_percent_df.to_dict(orient='index')
+        return thirty_percent_dict
