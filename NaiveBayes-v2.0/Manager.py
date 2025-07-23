7fce782ae640771940901d60bc1bb6df8deabcf1
mosheshulman <msh36345@gmail.com>
2025-07-23 17:11:08 +0300

    fixing duplicate run

--- NaiveBayes-v2.0/Manager.py
+++ NaiveBayes-v2.0/Manager.py
@@ -6,6 +6,7 @@ from Log import log
 
 class Manager:
     def __init__(self,path,dic_of_row):
+        log("manager is running...")
         self.loader = Loader(path)
         self.cleaner = Cleaner(self.loader.get_data())
         self.clean_data = self.cleaner.get_clean_data()
@@ -13,12 +14,10 @@ class Manager:
         self.accuracy = self.validator.tester()
         self.classifier = Classifier(self.clean_data)
         self.result = self.classifier.run_validation(dic_of_row)
-        self.run()
 
     def run(self):
-        log("manager is running...")
         answer = f"The analysis was {self.accuracy}% accurate.\nPredicted answer probability:\n"
         for probability,percentages in self.result.items():
-            answer+=f"{probability} : {percentages}\n"
+            answer+=f"          {probability} : {percentages}\n"
         return answer
 
