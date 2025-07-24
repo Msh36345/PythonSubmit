255fd348b37bd8930cb5f7bd8fad37619829af10
mosheshulman <msh36345@gmail.com>
2025-07-24 17:07:30 +0300

    NaiveBayes-v2.0

--- NaiveBayes-v2.0/docker-client/Manager.py
+++ NaiveBayes-v2.0/docker-client/Manager.py
@@ -12,17 +12,16 @@ class Manager:
         self.dic_of_row = dic_of_row
 
     def run_testing(self):
-        answer = f"The analysis was {self.request.my_dict["validator"]}% accurate.\nPredicted answer probability:\n"
+        answer = f"The analysis was {self.request.my_dict['validator']}% accurate.\nPredicted answer probability:\n"
         for probability,percentages in self.result.items():
             answer+=f"          {probability} : {percentages}\n"
         return answer
 
     def run(self):
-        answer = f"<h3>The analysis was {self.request.my_dict["validator"]}% accurate for titanic data.</h3>"
+        answer = f"<h3>The analysis was {self.request.my_dict['validator']}% accurate for titanic data.</h3>"
         answer += f"<h4>row to check : {self.dic_of_row}</h4><ul>"
         answer += "<h4>Predicted answer probability:</h4><ul>"
         for probability, percentage in self.result.items():
             answer += f"<li><b>{probability}</b> : {percentage}</li>"
         answer += "</ul>"
         return answer
-
--- NaiveBayes-v2.0/docker-client/RequestData.py
+++ NaiveBayes-v2.0/docker-client/RequestData.py
@@ -4,7 +4,7 @@ import ast
 
 class RequestData:
     def __init__(self):
-        self.url = 'http://127.0.0.1:8080'
+        self.url = 'http://127.0.0.1:8070'
         self.response = requests.get(self.url)
         self.dic = json.loads(self.response.text)
         self.my_dict = ast.literal_eval(self.dic)
