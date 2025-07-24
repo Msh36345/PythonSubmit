2e347d0d26b1be6563151d9a1b1fa6beb81bb0af
mosheshulman <msh36345@gmail.com>
2025-07-24 16:37:00 +0300

    Add docker-client folder to NaiveBayes-v2

---
+++ NaiveBayes-v2.0/docker-client/Manager.py
@@ -0,0 +1,28 @@
+from RequestData import RequestData
+from Classifier import Classifier
+from Log import log
+
+class Manager:
+    def __init__(self,dic_of_row):
+        log("manager client is running...")
+        self.request = RequestData()
+        self.calculate_data= self.request.my_dict["calculate_data"]
+        self.classifier = Classifier()
+        self.result = self.classifier.run_validation(self.calculate_data,dic_of_row,self.request.my_dict["col_to_fill"])
+        self.dic_of_row = dic_of_row
+
+    def run_testing(self):
+        answer = f"The analysis was {self.request.my_dict["validator"]}% accurate.\nPredicted answer probability:\n"
+        for probability,percentages in self.result.items():
+            answer+=f"          {probability} : {percentages}\n"
+        return answer
+
+    def run(self):
+        answer = f"<h3>The analysis was {self.request.my_dict["validator"]}% accurate for titanic data.</h3>"
+        answer += f"<h4>row to check : {self.dic_of_row}</h4><ul>"
+        answer += "<h4>Predicted answer probability:</h4><ul>"
+        for probability, percentage in self.result.items():
+            answer += f"<li><b>{probability}</b> : {percentage}</li>"
+        answer += "</ul>"
+        return answer
+
---
+++ NaiveBayes-v2.0/docker-client/Server.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from Manager import Manager
+from fastapi.responses import HTMLResponse
+import uvicorn
+
+
+dic = {"Pclass": 3,  "Sex": "male", "Age": 22, "SibSp": 0, "Parch": 0, "Fare": 7.25, "Embarked": "S"}
+manager = Manager(dic)
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+async def root():
+    return manager.run()
+
+if __name__ == "__main__":
+    uvicorn.run(app,host="127.0.0.1",port=8090)
