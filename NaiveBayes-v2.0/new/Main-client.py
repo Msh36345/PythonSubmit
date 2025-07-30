df63c9a17ace7a7e141b834df2d9647e01b3af3d
mosheshulman <msh36345@gmail.com>
2025-07-30 12:36:34 +0300

    push only NaiveBayes-v2.0 folder

--- NaiveBayes-v2.0/new/Main-client.py
+++
@@ -1,8 +0,0 @@
-from Manager import Manager
-
-
-path = "titanic.csv"
-
-dic = {"Pclass": 3,  "Sex": "male", "Age": 22, "SibSp": 0, "Parch": 0, "Fare": 7.25, "Embarked": "S"}
-manager = Manager(dic)
-print(manager.run_testing())
